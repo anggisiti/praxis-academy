398888ef2da3d23dcbb9a4f9324f1d7fb8426519
anggisiti <111473466+anggisiti@users.noreply.github.com>
2022-08-24 11:23:59 +0700

    Delete kemampuan-dasar/02-02 directory

--- kemampuan-dasar/02-02/4.5. pass Statements¶/index.py
+++
@@ -1,9 +0,0 @@
-while True:
-    pass  # Busy-wait for keyboard interrupt
-
-    class MyEmptyClass:
-        pass
-
-        def initlog(*args):
-            pass   # Remember to implement this!
-            
