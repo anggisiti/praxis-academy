db54c6a1c879e44d4e19e8bff3f75b13129d13cd
646.anggisiti.smkslf@gmail.com <646.anggisiti.smkslf@gmail,com>
2022-09-05 12:56:12 +0700

    teks print

--- notice/03-03/app/hello2.py
+++ notice/03-03/app/hello2.py
@@ -1,34 +1,17 @@
-from flask import Flask, url_for
+from Flask import Flask, url_for, request 
 from markupsafe import escape
 
-app = Flask(__name__)
+app = Flask(__nama__)
 
 @app.route('/')
 def index():
-    return 'index'
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    print(request.method)
-    if request.method == ""
+    return "index"
 
 @app.route('/login')
 def login():
-   
+    print(request.method)
+    return 'login'
+
 @app.route('/user/<username>')
 def profile(username):
-    return '{}\'s profile'.format(escape(username))
-
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
-
-if __name__ == "__main__":
-    app.run()
-'''
-Create  = POST
-Read    = GET
-Update  = PUT
-Delete  = DELETE
-'''
+    return '{}'\
