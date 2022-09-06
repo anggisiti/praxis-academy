a898643149849c02ed1dc60effe7f0cf6e5919c2
646.anggisiti.smkslf@gmail.com <646.anggisiti.smkslf@gmail,com>
2022-09-06 11:45:42 +0700

    teks print

--- notice/03-05/app/main.py
+++ notice/03-05/app/main.py
@@ -67,7 +67,7 @@ def delete(buah_id):
     conn.close()
     return redirect("/")
 
-@app.route("/update/<buah_id>")
+@app.route("/update/<buah_id>", methods=["GET", "POST"])
 def update(buah_id):
     conn = psycopg2.connect(
         host="localhost",
@@ -76,19 +76,24 @@ def update(buah_id):
         password="anggisiti"
     )
     curs = conn.cursor()
+    if request.method == "POST":
+        nama = request.form.get("nama")
+        detail = request.form.get("detail")
+        query = f"update buah set nama = '{nama}', detail = '{detail}' where id = {buah_id}"
+        curs.execute(query)
+        conn.commit()
+        return redirect("/")
+        # print(nama, detail)
     
-    namaLama = 'mangga'
-    namaBaru = 'apel'
-    detailBaru = 'sepet'
-
-    query = f"update buah set nama='{namaBaru}', detail='{detailBaru}' where nama ='{namaLama}'"
+    # namaLama = 'durian'
+    # namaBaru = 'duku'
+    # detailBaru = 'masam'
+    query = f"select * from buah where id = '{buah_id}'"
     curs.execute(query)
-    conn.commit()
+    data = curs.fetchone()
+    conn.commit
     print("data masuk")
-
-    return redirect("/")
-
-
+    return render_template("update.html", context=data)
 
 if __name__ == "__main__":
     app.run()
